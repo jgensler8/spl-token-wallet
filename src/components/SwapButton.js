6db26d3c5d3a0d67ae87327e64f2157386c200bc
armaniferrante <armaniferrante@gmail.com>
2021-05-21 02:44:41 -0700

    Keep swap popover mounted

--- src/components/SwapButton.js
+++ src/components/SwapButton.js
@@ -16,36 +16,39 @@ export default function SwapButton() {
   const connection = useConnection();
   const wallet = useWallet();
   const tokenInfos = useTokenInfos();
-  const tokenList = new TokenListContainer(tokenInfos);
+  const tokenList = tokenInfos && new TokenListContainer(tokenInfos);
   const provider = new NotifyingProvider(connection, wallet, sendTransaction);
 
   return (
-    <PopupState variant="popover">
-      {(popupState) => (
-        <div style={{ display: 'flex' }}>
-          <Tooltip title="Swap Tokens">
-            <IconButton {...bindTrigger(popupState)}>
-              <SwapHoriz />
-            </IconButton>
-          </Tooltip>
-          <Popover
-            {...bindPopover(popupState)}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            PaperProps={{ style: { borderRadius: '10px' } }}
-            disableRestoreFocus
-          >
-            <Swap provider={provider} tokenList={tokenList} />
-          </Popover>
-        </div>
-      )}
-    </PopupState>
+    tokenList && (
+      <PopupState variant="popover">
+        {(popupState) => (
+          <div style={{ display: 'flex' }}>
+            <Tooltip title="Swap Tokens">
+              <IconButton {...bindTrigger(popupState)}>
+                <SwapHoriz />
+              </IconButton>
+            </Tooltip>
+            <Popover
+              {...bindPopover(popupState)}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              PaperProps={{ style: { borderRadius: '10px' } }}
+              disableRestoreFocus
+              keepMounted
+            >
+              <Swap provider={provider} tokenList={tokenList} />
+            </Popover>
+          </div>
+        )}
+      </PopupState>
+    )
   );
 }
 
